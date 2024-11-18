738f89d58e1289dd1be37cb7893dfd83489a90da
Nikita1567 <rzhnik@gmail.com>
2024-11-18 16:54:20 +0500

    Second Commit (backend)
    routing. CRUD for products, types and brands. static, pagination, filtration.  USER CRUD, registration, autorisation, jwt token. Middleware.

---
+++ online-store/server/routes/productRouter.js
@@ -0,0 +1,10 @@
+const Router = require('express')
+const productController = require('../controllers/productController')
+const checkRole = require('../middleware/checkRoleMiddleware')
+const router = new Router()
+
+router.post('/', checkRole('ADMIN'), productController.create)
+router.get('/', productController.getAll)
+router.get('/:id', productController.getOne)
+
+module.exports = router
